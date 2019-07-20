bb0a5bf4b151e935e081c8c0a650b0a69f9a4555
toshe4kaMatsyshyn <50492862+toshe4kaMatsyshyn@users.noreply.github.com>
2019-07-20 09:48:54 +0300

    Создание класса Graph
    Создание основного класса, в котором будет реализован метод нахождения кратчайшего пути

---
+++ Dijkstra Algorihm/Dijkstra Algorihm/Graph.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dijkstra_Algorihm
+{
+    class Graph
+    {
+        /// <summary>
+        /// Матрица смежности
+        /// </summary>
+        int[,] graph;
+
+        /// <summary>
+        /// Массив для восстановления кратчайшего пути
+        /// </summary>
+        int[] FinalWay;
+
+        /// <summary>
+        /// Стоимость кратчайшего пути
+        /// </summary>
+        int FinalWeight;
+        Graph(int[,] graph)
+        {
+            this.graph = graph;
+            FinalWay = new int[graph.GetLength(0)];
+        }
+    }
+}
